86b897e550dbc1c7499a2989d1da85d0cfff4d5f
Oliver Mulcahy <omulcahy@ucsc.edu>
2020-04-09 15:58:02 -0700

    more work on webapp

--- webapp/app.py
+++ webapp/app.py
@@ -1,1 +1,12 @@
+# Guide from: https://projects.raspberrypi.org/en/projects/python-web-server-with-flask/1
+
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'Hello friends!'
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
