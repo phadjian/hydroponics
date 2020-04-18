03573d18acf1fef174a80260a1e0889e05970430
DiabayNottoway <51923624+DiabayNottoway@users.noreply.github.com>
2020-04-18 10:28:33 -0700

    Starting code of ecReader.py and ecSim.py, modifying sensorTester.py

--- testingAndReadingSensors/sensorTester.py
+++ testingAndReadingSensors/sensorTester.py
@@ -12,13 +12,19 @@ from waterLevelSim import steadyWaterLevelSim
 from waterLevelReader import waterLevel
 from tempHumidityReader import randomTempHumidity
 from tempHumidityReader import steadyTempHumidity
+from ecSim import randomECSim
+from ecReader import ec
+
+
 
 for x in range(0, 24):
 	randomWater = waterLevel(randomWaterLevelSim())
 	steadyWater = waterLevel(steadyWaterLevelSim())
 	randomTemp, randomHumidity = randomTempHumidity()
 	steadyTemp, steadyHumidity = steadyTempHumidity()
+	randomEC = ec(randomECSim())
 	print "random water level:", randomWater, "steady water level:", steadyWater
 	print "random temp and humidity: temperature = %0.1f, humidity = %0.1f" %(randomTemp, randomHumidity)
 	print "steady temp and humidity: temperature = %0.1f, humidity = %0.1f" %(steadyTemp, steadyHumidity)
+	print "ec:", randomEC
 	time.sleep(1)
